b957586cb42dcd8cd789551c6e866042436e2bce
LAPTOP - Oliver Maglana <jamols09@gmail.com>
2023-04-01 23:31:29 +0800

    fill up help component

--- quasar-project/src/pages/Help.vue
+++ quasar-project/src/pages/Help.vue
@@ -1,6 +1,19 @@
 <template>
-  <q-page>
-    <h5>Help</h5>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis quas
+      praesentium dolorem laudantium doloribus, omnis necessitatibus optio
+      sapiente odio voluptate dolorum. Amet quos quam architecto consectetur
+      laudantium, ut aliquid similique?
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium
+      nesciunt soluta iusto deserunt consectetur maxime. Ea corrupti eligendi
+      suscipit, dolorem quibusdam deserunt recusandae nisi optio libero eaque
+      delectus. Temporibus, provident!
+    </p>
   </q-page>
 </template>
 
