43b2d0d2d52789ba4d68230bfd789b1d5cb85cf9
LAPTOP - Oliver Maglana <jamols09@gmail.com>
2023-04-01 22:59:16 +0800

    display tasks, delete and pop-up dialog

--- quasar-project/src/pages/ToDo.vue
+++ quasar-project/src/pages/ToDo.vue
@@ -1,13 +1,88 @@
 <template>
-  <q-page>
-    <h5>To do</h5>
+  <q-page class="bg-grey-3 column">
+    <q-list class="bg-white" separator bordered>
+      <q-item
+        v-for="(task, index) in tasks"
+        :key="task.title"
+        v-ripple
+        @click="onDone(task)"
+        :class="task.done === true ? 'done bg-blue-1' : null"
+        clickable
+      >
+        <q-item-section avatar>
+          <q-checkbox
+            v-model="task.done"
+            color="primary"
+            class="no-pointer-events"
+          />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section v-if="task.done" side>
+          <q-btn
+            @click.stop="onDelete(index)"
+            flat
+            round
+            dense
+            color="primary"
+            icon="delete"
+          />
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { useQuasar } from "quasar";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "ToDo",
+
+  setup() {
+    const $q = useQuasar();
+    const tasks = ref([
+      { title: "Get bananas", done: false },
+      { title: "Eat bananas", done: false },
+      { title: "Poo bananas", done: false },
+    ]);
+
+    const confirmBtn = ref(false);
+
+    const onDone = (value) => {
+      value.done = !value.done;
+    };
+
+    const onDelete = (value) => {
+      $q.dialog({
+        title: "Confirm",
+        message: "Are you sure you want to delete?",
+        cancel: true,
+        persistent: true,
+      }).onOk(() => {
+        tasks.value.splice(value, 1);
+      });
+    };
+
+    return {
+      confirmBtn,
+      onDone,
+      onDelete,
+      tasks,
+    };
+  },
 });
 </script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+</style>
